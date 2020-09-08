a8da128c444407526f6986128d5a51da95443c80
Vitor Avelino <vitoravelinosena1@gmail.com>
2020-09-08 20:36:05 -0300

    vai

--- client/src/api/apiService.js
+++ client/src/api/apiService.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
-const API_URL_Retieve = 'http://localhost:3000/api/transaction/retieve/';
-const API_URL_Delete = 'http://localhost:3000/api/transaction/deleteTrans/';
-const API_URL_Uptade = 'http://localhost:3000/api/transaction/updateTrans/';
-const API_URL_Create = 'http://localhost:3000/api/transaction/createTrans';
+const dotenv = require('dotenv');
+dotenv.config();
+const APP_PORT = process.env;
+const API_URL_Retieve = 'http://localhost:300958/api/transaction/retieve/';
+const API_URL_Delete = 'http://localhost:300958/api/transaction/deleteTrans/';
+const API_URL_Uptade = 'http://localhost:300958/api/transaction/updateTrans/';
+const API_URL_Create = 'http://localhost:300958/api/transaction/createTrans';
 // const API_URL_Retieve = 'http://localhost:3001/api/transaction/retieve/';
 // const API_URL_Delete = 'http://localhost:3001/api/transaction/deleteTrans/';
 // const API_URL_Uptade = 'http://localhost:3001/api/transaction/updateTrans/';
 // const API_URL_Create = 'http://localhost:3001/api/transaction/createTrans';
 
 async function getAllTransaction(period) {
+  console.log('port' + APP_PORT);
   try {
     return await axios.get(API_URL_Retieve + period);
   } catch (e) {
